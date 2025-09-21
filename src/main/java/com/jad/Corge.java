d4fe18841199493072cf6140b7c373c9035954b0
eloise <eloise.ansoult@student.junia.com>
2025-09-21 18:33:41 +0200

    FooBarBazQuxCorgeGrault

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -13,8 +13,4 @@ public class Corge {
     public void setFoo(Foo foo) {
         this.foo = foo;
     }
-    @Override
-    public String toString() {
-        return "Corge [foo=" + foo + "]";
-    }
 }
