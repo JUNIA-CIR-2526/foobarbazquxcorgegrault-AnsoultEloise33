5bdd5a256c99fb4f82767c7b76f207c9c4d28877
eloise <eloise.ansoult@student.junia.com>
2025-09-21 17:44:30 +0200

    FooBarBazQux

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -32,4 +32,18 @@ public class Foo {
     public void setQux(Qux qux) {
         this.qux = qux;
     }
+    public static void main(String[] args) {
+        Bar bar = new Bar();
+
+        Foo foo = new Foo(bar);
+
+        Baz baz1 = new Baz();
+        Baz baz2 = new Baz();
+        foo.addBaz(baz1);
+        foo.addBaz(baz2);
+
+        Qux qux = new Qux();
+        foo.setQux(qux);
+
+    }
 }
